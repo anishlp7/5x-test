0a3ae3371034a6e9cd8ec2e24c2cebdf4241603e
anishlp7 <anishlp7@gmail.com>
2022-05-19 18:55:10 +0530

    Initalized the sass preprocessor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Styles/main.scss';
 
 const App = () => {
     return(
-        <h1>Hello World!</h1>
+        <h1 className='text123'>Hello World!</h1>
     );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const prod = process.env.NODE_ENV === 'production';
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: prod ? 'production' : 'development',
@@ -20,9 +19,16 @@ module.exports = {
         use: 'ts-loader',
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      }
     ]
   },
   devtool: prod ? undefined : 'source-map',
@@ -30,6 +36,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
-    new MiniCssExtractPlugin(),
+    //new MiniCssExtractPlugin(),
   ],
 };
